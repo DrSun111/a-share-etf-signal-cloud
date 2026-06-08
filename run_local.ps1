f74c8ad204a62228fbc95ae2365e19b9efc78676
DrSun111 <2805051912@qq.com>
2026-06-08 10:59:20 +0800

    Add run_local.ps1

---
+++ run_local.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = "Stop"
+Set-Location $PSScriptRoot
+
+if (!(Test-Path ".venv")) {
+    python -m venv .venv
+}
+
+.\.venv\Scripts\python -m pip install -r requirements.txt
+.\.venv\Scripts\python -m streamlit run app.py --server.port 8501
